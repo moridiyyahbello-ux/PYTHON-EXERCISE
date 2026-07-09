0dcf079aa3f0df8ba28b74df46be9fa2fe646694
Odunayo <moridiyyahbello@gmail.com>
2026-07-09 16:04:03 +0100

    phyton level one

---
+++ variables.py
@@ -0,0 +1,66 @@
+#LEVEL 1: VARIABLES
+first_name = 'Bello'
+last_name = 'Moridiyyah'
+full_name = first_name + ' ' + last_name
+country = 'Nigeria'
+city = 'Lagos'
+age = 25
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+Name, Age, Country, City = 'Bello', 25, 'Nigeria', 'Lagos'
+
+
+#LEVEL 2 number 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+#LEVEL 2 number 2
+print(len(first_name))
+
+#LEVEL 2 number 3
+print(len(first_name) > len(last_name))
+
+#LEVEL 2 number 4
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = 3.14 * (radius ** 2)
+circum_of_circle = 2 * 3.14 * radius
+take_area = int(input('Input your Radius:'))
+print(3.14* (radius**2))
+user_Firstname = input('Enter your Fisrt name:')
+user_Lastname = input('Enter your last name:')
+user_Country = input('Kindly enter your Country:')
+user_Age = input('Please Enter your Age:')
+print(num_one)
+print(num_two)
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+print(area_of_circle)
+print(circum_of_circle)
+print(user_Firstname)
+print(user_Lastname)
+print(user_Country)
+print(user_Age)
